59a2de3f53eb8a6a8e0406ade0af0bc92d4eb4e4
Prafuldev V R <thepzy64@gmail.com>
2018-12-07 23:25:39 +0530

    Success

--- app/src/main/java/customslider/pzy64/customslider/slider/CustomSlider.kt
+++ app/src/main/java/customslider/pzy64/customslider/slider/CustomSlider.kt
@@ -11,14 +11,16 @@ class CustomSlider @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
+
     private var viewWidth = 0
     private var viewHeight = 0
     private var onSeekListener: OnSeekListener? = null
 
-    private var touchX = 0f
+    private var touchX = 80f
     private var barStroke = 5f
     private var hemiCircleRadius = 50f
     private var innerCircleRadius = 40f
+    private var bend = 15f
 
     private var lineColor = "#424242".toColor()
     private var circleColor = "#212121".toColor()
@@ -34,29 +36,35 @@ class CustomSlider @JvmOverloads constructor(
         isAntiAlias = true
     }
 
-private var backgroundPaint = Paint().apply {
-    color = Color.WHITE
-    strokeWidth = barStroke + 5
-    isAntiAlias = true
-}
+    private var backgroundPaint = Paint().apply {
+        color = Color.WHITE
+        strokeWidth = barStroke + 5
+        isAntiAlias = true
+    }
 
-init {
-}
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
 
-override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-    super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        viewWidth = MeasureSpec.getSize(widthMeasureSpec) - paddingLeft - paddingRight
+        viewHeight = MeasureSpec.getSize(heightMeasureSpec) - paddingTop - paddingBottom
 
-    viewWidth = MeasureSpec.getSize(widthMeasureSpec) - paddingLeft - paddingRight
-    viewHeight = MeasureSpec.getSize(heightMeasureSpec) - paddingTop - paddingBottom
+    }
 
-}
+    override fun onDraw(c: Canvas?) {
+        super.onDraw(c)
+        c?.let { canvas ->
+            canvas.save()
 
-override fun onDraw(c: Canvas?) {
-    super.onDraw(c)
-    c?.let { canvas ->
-        canvas.save()
+            canvas.drawPaint(backgroundPaint)
 
-        canvas.drawPaint(backgroundPaint)
+            drawShape(canvas)
+
+            canvas.restore()
+        }
+    }
+
+
+    fun drawShape(canvas: Canvas) {
 
         val oval = RectF().apply {
             left = touchX - hemiCircleRadius
@@ -66,60 +74,72 @@ override fun onDraw(c: Canvas?) {
         }
 
 
+        val bendRight = RectF().apply {
+            left = oval.right
+            right = oval.right + bend
+            bottom = viewHeight / 2f
+            top = viewHeight / 2f - bend
+        }
 
-        canvas.drawArc(oval, 0f, -180f, true, linePaint)
+        val bendLeft = RectF().apply {
+            left = oval.left - bend
+            right = oval.left
+            bottom = viewHeight / 2f
+            top = viewHeight / 2f - bend
+        }
 
-        canvas.drawLine(0f, viewHeight / 2f, oval.left, viewHeight / 2f, linePaint)
 
-        canvas.drawLine(oval.left + 2, viewHeight / 2f + 2, oval.right - 2, viewHeight / 2f + 2, backgroundPaint)
+        canvas.drawLine(0f, viewHeight / 2f, oval.left - bend / 2, viewHeight / 2f, linePaint)
+        canvas.drawLine(oval.right + bend / 2, viewHeight / 2f, viewWidth.toFloat(), viewHeight / 2f, linePaint)
 
-        canvas.drawLine(oval.right, viewHeight / 2f, viewWidth.toFloat(), viewHeight / 2f, linePaint)
+
+        val path = Path()
+        path.addArc(bendLeft, 90f, -90f)
+        path.arcTo(oval, 200f, 140f)
+        path.arcTo(bendRight, 180f, -90f)
+
+
+        canvas.drawPath(path, linePaint)
 
         canvas.drawCircle(touchX, viewHeight / 2f, innerCircleRadius, paint)
-
-
-
-
-        canvas.restore()
     }
-    invalidate()
-}
 
-fun setOnSeekListener(listener: OnSeekListener) {
-    onSeekListener = listener
-}
+    fun setOnSeekListener(listener: OnSeekListener) {
+        onSeekListener = listener
+    }
 
-override fun onTouchEvent(event: MotionEvent?): Boolean {
-    event?.let {
-        when (event.action) {
-            MotionEvent.ACTION_DOWN -> {
-                Log.d("YYY", "down")
-            }
-            MotionEvent.ACTION_UP -> {
-                Log.d("YYY", "up")
-            }
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        event?.let {
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    Log.d("YYY", "down")
+                }
+                MotionEvent.ACTION_UP -> {
+                    Log.d("YYY", "up")
+                }
 
-            MotionEvent.ACTION_MOVE -> {
-                touchX = event.x
-            }
-            else -> {
-            }
+                MotionEvent.ACTION_MOVE -> {
+                    touchX = event.x
+                    invalidate()
+                }
+                else -> {
+                }
 
+            }
         }
+        return true
     }
-    return true
-}
 
-override fun performClick(): Boolean {
-    Log.d("YYY", "clicked")
-    return super.performClick()
+    override fun performClick(): Boolean {
+        Log.d("YYY", "clicked")
+        return super.performClick()
 
-}
+    }
 
 
-/**
- * Color converter
- * */
-fun String.toColor() = Color.parseColor(this)
+    /**
+     * Color converter
+     * */
+    fun String.toColor() = Color.parseColor(this)
 
 }
